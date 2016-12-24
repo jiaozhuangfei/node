8e2d7ad49fb4f56ac4f81645668a1ed24c083903
zhufengnodejs <zhufengnodejs@126.com>
2016-12-24 22:06:26 +0800

    用express实现路由

--- 1.express/login/app.js
+++ 1.express/login/app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+//设置模板引擎
+app.set('view engine');
+//设置模板的存放目录
+app.set('views');
 //注册
-app.get('/signup');
+app.get('/signup',function(req,res){
+    //模板的相对路径  数据对象
+    res.render('signup',{title:'注册'});
+});
 app.post('/signup');
 //登录
 app.get('/signin');
