27ef05e2359ec9053bdfacd389c85745cb01757e
zhufengnodejs <zhufengnodejs@126.com>
2016-12-25 18:25:12 +0800

    用express实现路由

--- 4.session/2.express.js
+++ 4.session/2.express.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var path = require('path');
 var session = require('express-session');
 var app = express();
+app.set('view engine','html');
+app.set('views',path.resolve('views'));
+app.engine('html',require('ejs').__express);
 //当你使用session中间件之后，会在req.session属性
 //req.session就是此客户端在服务器端对应的数据对象
 app.use(session({
@@ -10,7 +14,8 @@ app.use(session({
 }));
 app.get('/write',function(req,res){
     //给req.session属性赋值就相当于写session
-   req.session.name = 'zfpx';
+   //req.session.name = 'zfpx';
+   req.session.success = '注册成功';
    res.send('ok');
 });
 app.get('/read',function(req,res){
@@ -24,5 +29,9 @@ app.get('/hair',function(req,res){
         req.session.money-=20;
         res.send("您卡内剩余"+req.session.money+"元");
     }
+});
+app.get('/user/signup',function(req,res){
+    res.render('signup',{error:'',
+    success:req.session.success});
 });
 app.listen(8080);
