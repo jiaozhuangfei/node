779bddfc1da8efe4c4179f45480d638be7740bbe
zhufengnodejs <zhufengnodejs@126.com>
2016-12-28 12:43:28 +0800

    用express实现路由

--- 5.es6/6.class.js
+++ 5.es6/6.class.js
@@ -14,7 +14,7 @@ class Person{
    }
 }
 var p1 = new Person('zfpx');
-p1.getName();
+//p1.getName();
 //类的继承
 class Student extends Person{
   constructor(name,age){
@@ -26,29 +26,35 @@ class Student extends Person{
   }
 }
 let s1 = new Student('zfpx',8);
-s1.getName();
-s1.getAge();
+//s1.getName();
+//s1.getAge();
 //====================es6===========================
-function Person5(name){
+function Person5(name) {
     this.name = name;
 }
-Person5.prototype = {
-    constructor:Person5,
-    getName:function(){
-        console.log(this.name);
-    }
-}
-Person5.prototype.getName = function(){
+Person5.prototype.getName = function (){
     console.log(this.name);
+};
+var p2 = new Person5();
+//p2.getName();
+
+function Student5(name,age){
+    //调用父类的构造函数 this指向当前子类的实例
+    Person5.apply(this,arguments);
+    this.age = age;
 }
-var p5 = new Person5('zfpx');
-p5.getName();
-
-
-function Student5(){
-
+//基于原型创建一个实例出来
+function create(prototype){
+   function Fn(){}
+   Fn.prototype = prototype;
+   return new Fn();
 }
-
-let s5 = new Student('zfpx',8);
+Student5.prototype = create(Person5.prototype);
+Student5.prototype.constructor = Student5;
+Student5.prototype.getAge = function (){
+    console.log(this.age);
+};
+var s5 = new Student5('zfpx',8);
 s5.getName();
 s5.getAge();
+
