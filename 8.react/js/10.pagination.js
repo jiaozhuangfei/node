3bbc3c8b24d764e1ca47179b8c52ca0a243d81e7
zhufengnodejs <zhufengnodejs@126.com>
2016-12-29 15:48:47 +0800

    reac

--- 8.react/js/10.pagination.js
+++ 8.react/js/10.pagination.js
@@ -1,18 +1,24 @@
 //分页组件
+/**
+ * 1. total指的是总页数，number指的是当前页码
+ * 2. total指的是要显示多少个li,number是几，那么对应的li要增加active样式
+ * 3. 如果当前页是第一页，不显示上一页
+ *    如果当前页最后一页，则不显示下一页
+ */
 let Pagination = React.createClass({
    render(){
+       let pages = [];
+       for(let i=1;i<=this.props.total;i++){
+           pages.push(<Page key={i}></Page>);
+       }
        return (
            <nav>
                <ul className="pagination">
-                   <Page/>
-                   <Page/>
-                   <Page/>
-                   <Page/>
-                   <Page/>
-                   <Page/>
+                   {
+                       pages
+                   }
                </ul>
            </nav>
-
        )
    }
 });
