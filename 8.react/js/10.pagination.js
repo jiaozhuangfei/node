830ab385db8b078d0adb7274edd4bc073259a639
zhufengnodejs <zhufengnodejs@126.com>
2016-12-29 16:29:30 +0800

    reac

--- 8.react/js/10.pagination.js
+++ 8.react/js/10.pagination.js
@@ -14,15 +14,19 @@ let Pagination = React.createClass({
        this.setState({number});
    },
    render(){
-       let pages = [];
-       if(this.state.number > 1){
-           pages.push(<Page changeNumber={this.changeNumber} current={this.state.number-1} class={''} key={0}>&laquo;</Page>);
+       let pages = [];//存放所有分页按钮
+       if(this.state.number > 1){//如果当前页大于第1页
+           //current 是如果点击此分页按钮，跳转到哪一页=当前页减去1
+           //class因为永远不要高亮，所以给个空即可。key只要不跟别的分页按钮冲突即可。0
+           pages.push(<Page changeNumber={this.changeNumber} target={this.state.number-1} class={''} key={0}>&laquo;</Page>);
        }
+       //循环总页码，往数组中放总页码个分页按钮
        for(let i=1;i<=this.props.total;i++){
-           pages.push(<Page changeNumber={this.changeNumber}  class={this.state.number==i?'active':''} key={i} current={i}>{i}</Page>);
+           pages.push(<Page changeNumber={this.changeNumber}  class={this.state.number==i?'active':''} key={i} target={i}>{i}</Page>);
        }
+       //如果当前页小于总页码
        if(this.state.number < this.props.total){
-           pages.push(<Page current={this.state.number+1} changeNumber={this.changeNumber} class={''} key={-1}>&raquo;</Page>);
+           pages.push(<Page target={this.state.number+1} changeNumber={this.changeNumber} class={''} key={-1}>&raquo;</Page>);
        }
        return (
            <nav>
@@ -37,8 +41,10 @@ let Pagination = React.createClass({
 });
 let Page  = React.createClass({
     render(){
-        return <li onClick={this.props.changeNumber.bind(null,this.props.current)} className ={this.props.class}><a href="#">{this.props.children}</a></li>;
+        return <li onClick={()=>{
+            this.props.changeNumber(this.props.current)}
+        } className ={this.props.class}><a href="#">{this.props.children}</a></li>;
     }
 });
 
-ReactDOM.render(<Pagination total={5} number={5}/>,document.querySelector('#container'));
+ReactDOM.render(<Pagination total={5} number={3}/>,document.querySelector('#container'));
