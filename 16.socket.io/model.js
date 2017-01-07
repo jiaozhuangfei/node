56170564dc8bc3b5b9272941051251fb34d0fb5d
zhufengnodejs <zhufengnodejs@126.com>
2017-01-07 15:10:01 +0800

    reac

---
+++ 16.socket.io/model.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose');
+//连接数据库
+mongoose.connect('mongodb://127.0.0.1/201611chat');
+//定义schema
+let MessageSchema = new mongoose.Schema({
+   content:{type:String,required:true}
+});
+//定义模型并导出
+module.exports.Message = mongoose.model('Message',MessageSchema);
