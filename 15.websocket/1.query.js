a91b1fa75863790d67a41b7c96c5c6dfb3693f72
zhufengnodejs <zhufengnodejs@126.com>
2017-01-07 10:55:59 +0800

    reac

--- 15.websocket/1.query.js
+++ 15.websocket/1.query.js
@@ -4,7 +4,10 @@ app.use(function(req,res,next){
   res.setHeader('Access-Control-Allow-Origin','http://localhost:63342');
   next();
 });
+let start  = Date.now();
 app.get('/clock',function(req,res){
   res.send(new Date().toLocaleString());
+  console.log(Date.now() - start);
+  start = Date.now();
 });
 app.listen(8080);
