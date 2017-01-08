85a88c79509954517e1038f87ba7df27c55d7e5e
zhufengnodejs <zhufengnodejs@126.com>
2017-01-08 16:59:28 +0800

    reac

--- 18.crawl/tasks/main.js
+++ 18.crawl/tasks/main.js
@@ -1,7 +1,7 @@
 let async = require('async');
 let url = 'http://top.baidu.com/buzz?b=26&c=1&fr=topcategory_c1';
 let read = require('./read');
-let write = require('./write');
+let write = require('./write');//日志记录器
 let debug = require('debug')('201611crawl:main');
 //瀑布模型，上一个输出是下一个输入
 async.waterfall([
